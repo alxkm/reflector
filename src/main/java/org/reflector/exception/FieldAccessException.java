5162780ab01ae063e212ab8d3b769e8981a06dbd
alexkm <alexanderklmn@gmail.com>
2022-09-01 18:39:34 +0300

    Initial library files (#1)
    * Initial library files

---
+++ src/main/java/org/reflector/exception/FieldAccessException.java
@@ -0,0 +1,7 @@
+package org.reflector.exception;
+
+public class FieldAccessException extends RuntimeException {
+    public FieldAccessException(String cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/org/reflector/exception/InstanceInvocationException.java
@@ -0,0 +1,7 @@
+package org.reflector.exception;
+
+public class InstanceInvocationException extends RuntimeException {
+    public InstanceInvocationException(String cause) {
+        super(cause);
+    }
+}
