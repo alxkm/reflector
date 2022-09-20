e4f1b00aa9e212a6b08f5522bb5d7f2a9405eb12
Oleksandr Klymenko <alexanderklmn@gmail.com>
2022-09-20 19:13:17 +0300

    Test classes packages have been refactored

--- src/test/java/org/common/reflector/TestConstant.java
+++
@@ -1,4 +0,0 @@
-package org.common.reflector;
-
-public class TestConstant {
-}
--- src/test/java/org/common/reflector/data/ExtendedAnnotatedClass.java
+++ src/test/java/org/common/reflector/data/ExtendedAnnotatedClass.java
@@ -1,5 +1,7 @@
 package org.common.reflector.data;
 
+import org.common.reflector.data.annotation.ClassAnnotation;
+
 @ClassAnnotation
 public class ExtendedAnnotatedClass {
 }
