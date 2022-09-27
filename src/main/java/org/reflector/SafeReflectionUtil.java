06baa278ab7d152f6fd4b31fab46d8806adfc85e
Alex Klm <alexanderklmn@gmail.com>
2022-09-27 11:30:43 +0300

    Wrappers for simple class methods have been added (#43)

--- src/main/java/org/reflector/SafeReflectionUtil.java
+++ src/main/java/org/reflector/SafeReflectionUtil.java
@@ -14,6 +14,46 @@ public final class SafeReflectionUtil {
      * */
     private SafeReflectionUtil() {}
 
+    public static Optional<String> getClassFullName(final Object obj) {
+        try {
+            Optional.of(ReflectionUtil.getClassFullName(obj));
+        } catch (Exception ignored) {
+        }
+        return Optional.empty();
+    }
+
+    public static Optional<String> getClassCanonicalName(final Object obj) {
+        try {
+            Optional.of(ReflectionUtil.getClassCanonicalName(obj));
+        } catch (Exception ignored) {
+        }
+        return Optional.empty();
+    }
+
+    public static Optional<String> getClassSimpleName(final Object obj) {
+        try {
+            Optional.of(ReflectionUtil.getClassSimpleName(obj));
+        } catch (Exception ignored) {
+        }
+        return Optional.empty();
+    }
+
+    public static Optional<String> getPackage(final Object obj) {
+        try {
+            Optional.of(ReflectionUtil.getPackage(obj));
+        } catch (Exception ignored) {
+        }
+        return Optional.empty();
+    }
+
+    public static Optional<String> getClassFullNameByClass(final Class<?> clazz) {
+        try {
+            Optional.of(ReflectionUtil.getClassFullNameByClass(clazz));
+        } catch (Exception ignored) {
+        }
+        return Optional.empty();
+    }
+
     public static <T> Optional<T> readField(Object object, String fieldName) {
         try {
             Optional.of(ReflectionUtil.readField(object, fieldName));
