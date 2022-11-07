1306136808a10e10da614016af2ff59af8a58777
Oleksandr Klymenko <okly@telenor.dk>
2022-11-07 11:00:08 +0200

    Constants refactoring

--- src/main/java/org/reflector/util/ReflectionConstant.java
+++ src/main/java/org/reflector/util/ReflectionConstant.java
@@ -8,5 +8,7 @@ final public class ReflectionConstant {
 
     public static String CLASS = ".class";
     public static String DOT = ".";
+    public static char DOT_SYMBOL = '.';
+    public static char SLASH = '/';
     public static int CLASS_NAME_CONSTANT = 6;
 }
