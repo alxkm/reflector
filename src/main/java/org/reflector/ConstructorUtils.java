1f374437ec99768ea55cd105374dcc8b6c6ee016
Alex Klymenko <alexanderklmn@gmail.com>
2024-09-09 00:27:32 +0200

    fix redundant import (#101)

--- src/main/java/org/reflector/ConstructorUtils.java
+++ src/main/java/org/reflector/ConstructorUtils.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Modifier;
 import java.lang.reflect.Parameter;
 
 public final class ConstructorUtils {
